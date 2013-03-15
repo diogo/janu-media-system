a5832250bcd43754851e5cfcc3a0681b62b2cde6
Januário <janu@palmares.(none)>
2013-03-15 02:09:47 -0300

    Projeto criado.

---
+++ janu-media-manager/jmm.py
@@ -0,0 +1,77 @@
+
+
+class JanuMediaManager(object):
+	def __init__(self):
+		pass
+
+	def _load_media_sources(self):
+		pass
+
+	def _load_media_types(self):
+		pass
+
+	def _load_media_formats(self):
+		pass
+
+	def _load_lyrics_plugins(self):
+		pass
+
+	def set_database(self):
+		pass
+
+	def update_database(self):
+		pass
+
+	def add_media_source(self):
+		pass
+
+	def remove_media_source(self):
+		pass
+
+	def login(self):
+		pass
+
+	def logout(self):
+		pass
+
+	def get_playlists(self):
+		pass
+
+	def add_playlist(self):
+		pass
+
+	def remove_playlist(self):
+		pass
+
+	def get_media_types(self):
+		pass
+
+class Cover(object):
+	pass
+
+class Artist(object):
+	pass
+
+class Album(object):
+	pass
+
+class Genre(object):
+	pass
+
+class MediaFormat(object):
+	pass
+
+class MediaType(object):
+	pass
+
+class MediaSource(object):
+	pass
+
+class Playlist(object):
+	pass
+
+class User(object):
+	pass
+
+class Media(object):
+	pass
