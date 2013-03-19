ee9802076519b9f5216e8d30428226477332d5d9
Diogo Dutra <dda@thiamatal.local>
2013-03-19 20:11:15 -0300

    primeiro commit

---
+++ janu-media-manager/common.py
@@ -0,0 +1,30 @@
+
+class Cover(object):
+	pass
+
+class Artist(object):
+	pass
+
+class Album(object):
+	pass
+
+class Genre(object):
+	pass
+
+class MediaFormatModule(object):
+	pass
+
+class MediaSourceModule(object):
+	pass
+
+class MediaSource(object):
+	pass
+
+class Playlist(object):
+	pass
+
+class User(object):
+	pass
+
+class Media(object):
+	pass
