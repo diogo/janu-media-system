b6bf210b97411a3ace2c34121d8a92ac0f395c15
Diogo Dutra <diogodutra@nyxk.com.br>
2013-09-04 20:03:38 -0300

    content_types/audio_mpeg.py: agora ele sempre remove o arquivo depois de baixar. sempre retorna None caso não consiga extrair alguma informação obrigatória. está fazendo sempre 3 readlines pra garantir q vai pegar toda a tag.

--- janu-media-manager/jmm/content_types/audio_mpeg.py
+++ janu-media-manager/jmm/content_types/audio_mpeg.py
@@ -1,3 +1,4 @@
+import os
 import urllib2
 from datetime import datetime
 from mutagen.easyid3 import EasyID3
@@ -6,19 +7,25 @@ from mutagen.id3 import ID3NoHeaderError
 def get_media_dict(media_url):
 	file_name = '/tmp/jmm%d.mp3' % datetime.now().microsecond
 	mp3_file = open(file_name, 'w+')
-	mp3_file.write(urllib2.urlopen(media_url).readline())
+	media = urllib2.urlopen(media_url)
+	media_content = media.readline()
+	media_content += media.readline()
+	media_content += media.readline()
+	mp3_file.write(media_content)
 	mp3_file.close()
+	dict_ = None
 	try:
-		dict_ = {k: v[0] for k, v in EasyID3(file_name).items()}
+		mp3_tags = EasyID3(file_name)
+		dict_ = {k: v[0] for k, v in mp3_tags.items()}
 		if not dict_.has_key('artist'):
-			return None
+			raise ID3NoHeaderError()
 		if not dict_.has_key('genre'):
-			return None
+			raise ID3NoHeaderError()
 		if dict_.has_key('title'):
 			dict_['name'] = dict_['title']
 			dict_.pop('title')
 		else:
-			return None
+			raise ID3NoHeaderError()
 		genres = dict_['genre'].split(',')
 		dict_['genre'] = []
 		for genre in genres:
@@ -31,6 +38,12 @@ def get_media_dict(media_url):
 		if dict_.has_key('tracknumber'):
 			dict_['collection_position'] = dict_['tracknumber']
 			dict_.pop('tracknumber')
-		return dict_
 	except ID3NoHeaderError:
-		return None
+		dict_ = None
+	try:
+		os.remove(file_name)
+	except OSError:
+		pass
+	if dict_:
+		dict_['url'] = media_url
+	return dict_
